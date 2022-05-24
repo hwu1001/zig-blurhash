d510b82124b9523a2f3dc762982c10ab8894e623
Henry Wu <hywu925@gmail.com>
2022-05-24 01:18:20 -0700

    Export encode

--- blurhash.zig
+++ blurhash.zig
@@ -1,5 +1,6 @@
 pub const base83 = @import("src/base83.zig");
 pub const decode = @import("src/decode.zig");
+pub const encode = @import("src/encode.zig");
 
 test "pkg" {
     @import("std").testing.refAllDecls(@This());
